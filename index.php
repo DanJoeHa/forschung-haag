444728299b289e096c7ef242ae955b7d740f0f9c
j.haag <johannes.d.haag@web.de>
2015-09-28 09:46:36 +0200

    Initial

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+	
+	
+	//Ausgabe
+	include('template/header.php');
+	include('template/start.php');
+	include('template/footer.php');
+	
+?>
---
+++ template/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<head>
+	
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	
+	<title>Forschung Haag</title>
+	
+	<meta name="author" content="Johannes Haag" />
+	<meta name="description" content="Historische Forschung Simon M. Haag MA" />
+	<meta name="keywords" content="Simon M. Haag MA, Simon M. Haag, Simon, Haag, Forschung, Historische Forschung, Wappen" />
+	
+</head>
+<body>
+	
